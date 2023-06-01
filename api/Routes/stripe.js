a4d8e1c5ff577abbe30eed89c71d55bb6a04ea5d
PILAR <wer@wers-iMac.local>
2023-06-01 11:50:43 +0200

    client url

--- api/Routes/stripe.js
+++ api/Routes/stripe.js
@@ -36,8 +36,8 @@ router.post('/create-checkout-session', async (req, res) => {
     customer: customer.id,
     line_items,
       mode: 'payment',
-      success_url: `${process.env.CLIENT_URL}/checkout-success`,
-      cancel_url: `${process.env.CLIENT_URL}/cart`,
+      success_url: `https://bookcorner.onrender.com/checkout-success`,
+      cancel_url: `https://bookcorner.onrender.com/cart`,
     });
     res.send({url: session.url})
 })
